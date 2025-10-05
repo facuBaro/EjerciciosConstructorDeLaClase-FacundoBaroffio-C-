31565523895045c75923b0bc98aa9286b51ca7df
facuBaro <facufutbolbaroffio@gmail.com>
2025-10-05 15:20:35 -0300

    añadi todos los archivos

---
+++ Ejercicios Constructor de la clase c#/Ejercicio 1/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    class Empleados
+    {
+        private string nombre;
+        private int sueldo;
+
+        public Empleados()
+        {
+            string respuesta;
+            Console.WriteLine("Ingrese el nombre del empleado:");
+            respuesta = Console.ReadLine();
+            nombre = Convert.ToString(respuesta);
+            Console.WriteLine("Ingrese el sueldo del empleado:");
+            respuesta = Console.ReadLine();
+            sueldo = Convert.ToInt32(respuesta);
+
+
+
+        }
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Nombre del empleado: " + nombre);
+            Console.WriteLine("Sueldo del empleado: " + sueldo);
+        }
+        public void PagoImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("El empleado " + nombre + " debe pagar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("El empleado " + nombre + " no debe pagar impuestos");
+            }
+        }
+
+    }
+    private static void Main(string[] args)
+    {
+        /*1)Confeccionar una clase que represente un empleado. Definir como atributos su nombre y
+  su sueldo. En el constructor cargar los atributos y luego en otro método imprimir sus datos y
+  por último uno que imprima un mensaje si debe pagar impuestos (si el sueldo supera a
+  3000)*/
+        Empleados empleado = new Empleados();
+        empleado.ImprimirDatos();
+        empleado.PagoImpuestos();
+        Console.ReadKey();
+    }
+}
---
+++ Ejercicios Constructor de la clase c#/Ejercicio2/Ejer2/Ejer2/Program.cs
@@ -0,0 +1,75 @@
+﻿internal class Program
+{
+
+    class operaciones
+    {
+        private int numero1, numero2, resultadoSuma, resultadoResta, resultadoMultiplicar;
+        float resultadoDividir;
+        bool divisionValida;
+        public operaciones()
+        {
+            int leer;
+            Console.WriteLine("Ingrese el primer numero entero");
+            leer = Convert.ToInt32(Console.ReadLine());
+            numero1 = leer;
+            Console.WriteLine("Ingrese el segundo numero entero");
+            leer = Convert.ToInt32(Console.ReadLine());
+            numero2 = leer;
+        }
+        public void calculoSuma()
+        {
+            resultadoSuma = numero1 + numero2;
+        }
+        public void calculoResta()
+        {
+            resultadoResta = numero1 - numero2;
+        }
+        public void calculoMultiplicación()
+        {
+            resultadoMultiplicar = numero1 * numero2;
+        }
+        public void calculoDivisión()
+        {
+            if (numero2 != 0)
+            {
+                resultadoDividir = (Convert.ToSingle(numero1) / Convert.ToSingle(numero2));
+                divisionValida = true;
+            }
+            else
+            {
+
+                divisionValida = false;
+            }
+
+        }
+        public void MostrarResultados()
+        {
+            Console.WriteLine("Resultado de la suma: " + resultadoSuma);
+            Console.WriteLine("Resultado de la resta: " + resultadoResta);
+            Console.WriteLine("Resultado de la multiplicación: " + resultadoMultiplicar);
+            if (divisionValida == true)
+            {
+                Console.WriteLine("Resultado de la división: " + resultadoDividir);
+            }
+            else
+            {
+                Console.WriteLine("Resultado de la división: No se pudo efectuar debido a que se divide por 0.");
+            }
+
+        }
+    }
+    private static void Main(string[] args)
+    {
+        /*2)Implementar la clase operaciones. Se deben cargar dos valores enteros en el constructor,
+calcular su suma, resta, multiplicación y división, cada una en un método, imprimir dichos
+resultados.*/
+        operaciones operaciones = new operaciones();
+        operaciones.calculoSuma();
+        operaciones.calculoResta();
+        operaciones.calculoMultiplicación();
+        operaciones.calculoDivisión();
+        operaciones.MostrarResultados();
+        Console.ReadKey();
+
+    }
+}
